656b511f80199d1b0ffd220b612edf54643957a9
Breno Wiebbelling <brenowdea@gmail.com>
2022-01-18 19:22:42 -0300

    commit final de 18/01

---
+++ pags/cv/cv.js
@@ -0,0 +1,10 @@
+imgGitEnter = function(){
+    img = document.getElementById('gitImg')
+
+    img.setAttribute('src','files/git.png')
+}
+imgGitLeave = function(){
+    img = document.getElementById('gitImg')
+
+    img.setAttribute('src','files/gitblack.png')
+}
