504b6ffd75d24166d7b010f413c5f5ec56833025
breno-wiebbelling <82486373+breno-wiebbelling@users.noreply.github.com>
2022-01-19 14:46:34 -0300

    Add files via upload

---
+++ pags/cv/cv.js
@@ -0,0 +1,10 @@
+imgGitEnter = function(){
+    img = document.getElementById('gitImg')
+
+    img.setAttribute('src','files/logos/git.png')
+}
+imgGitLeave = function(){
+    img = document.getElementById('gitImg')
+
+    img.setAttribute('src','files/logos/gitblack.png')
+}
